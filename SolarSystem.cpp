56af5b872de65197e6beeccaf19425258c6013a0
francescodalessandro <48754858+francescodalessandro@users.noreply.github.com>
2019-04-30 12:53:33 +0200

    generazione casuale pianeti

--- SolarSystem.cpp
+++ SolarSystem.cpp
@@ -4,33 +4,24 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <vector>
-#include <random>
 #include "Camera.hpp"
 #include "Scene.hpp"
 #include "Planet.hpp"
 #include "Declarations.hpp"
 #include "SolarSystem.hpp"
 #include "Bunker.hpp"
-#include "TankBunker.hpp"
-#include "CleverBunker.hpp"
-#include "DoubleShootBunker.hpp"
-
 
 using namespace std;
 using namespace sf;
 
-
-
 const void cameraFunction1(Camera* camera, const float deltaTime, vector<Object*> *sceneObjects) {}
 
 SolarSystem::SolarSystem() : Scene(cameraFunction1){
+    srand(time(NULL));
     int nPlanets = 9;
     int nColumns = 3;
     this->addObject(new Spaceship());
-    this->addObject(new DoubleShootBunker());
-    //this->addObject(new TankBunker());
     this->Division(nPlanets,nColumns);
-    //vectorPlanets.push_back(new Planet(Vector2f(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)));
 }
 
 const void SolarSystem::Division(int nPlanets,int nColumns){
@@ -38,19 +29,17 @@ const void SolarSystem::Division(int nPlanets,int nColumns){
     int sizeBlocky= WINDOW_HEIGHT/nColumns;
     int xstart=0;
     int ystart=0;
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> disx(0, sizeBlockx);
-    uniform_int_distribution<> disy(0, sizeBlocky);
-    //int xend=0;
-    //int yend=0;
     for(int i=0; i< nPlanets/nColumns; i++){
         for(int j=0;j<nPlanets/nColumns;j++){
             xstart= j * sizeBlockx;
             ystart= i * sizeBlocky;
-            //xend= xstart + sizeBlockx;
-            //yend= ystart + sizeBlocky; 
-            vectorPlanets.push_back(new Planet(Vector2f(xstart + disx(gen), ystart + disy(gen))));
+            int emptyBlock= rand()%2;
+            if((i == 1 && j == 1) || (emptyBlock==0)){  
+                //aggiungere caso in cui i pianeti sono 0
+           }
+           else{
+           vectorPlanets.push_back(new Planet(Vector2f(rand()%sizeBlockx+ xstart,rand()%sizeBlocky+ ystart)));
+           }
         }
     }
 } 
