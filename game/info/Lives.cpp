a4ef3f4dd3f18a58939a7930fa57e1e66ba74696
francescodalessandro <fradale98@gmail.com>
2019-10-04 22:09:29 +0200

    Iniziate classi Score e Lives

--- game/info/Lives.cpp
+++ game/info/Lives.cpp
@@ -1,1 +1,10 @@
 #include "Lives.hpp"
+
+Lives::Lives(): Info(new Text(), Point(50,80)){
+
+    this->getText()->setString("prova2");
+    this->getText()->setColor(Color::White);
+    this->getText()->setCharacterSize(100);
+}
+
+void Lives::update(){}
--- game/info/Score.cpp
+++ game/info/Score.cpp
@@ -1,1 +1,12 @@
 #include "Score.hpp"
+#include "../../engine/Info.hpp"
+
+
+Score::Score() : Info(new Text(), Point(50, 50)){
+
+    this->getText()->setString("prova");
+    this->getText()->setColor(Color::White);
+    this->getText()->setCharacterSize(30);
+}
+
+void Score::update(){}
